b32da34f82bb6f67d2b54646ba87ffa8629f4c75
Brian Chrzanowski <chrzanowskitest@gmail.com>
2024-01-16 00:21:08 -0500

    redid wordle code

--- wordle.c
+++ wordle.c
@@ -14,9 +14,6 @@
 #include <ctype.h>
 #include <limits.h>
 
-#include "stb_ds.h"
-
-// #define DICTFILE "/usr/share/dict/american-english"
 #define DICTFILE "/usr/share/dict/american-english"
 #define LISTLIM  (1 << 20) // seems big enough :p
 #define WORDLELEN (5)
@@ -34,7 +31,7 @@
 #define DBG(FMT,...)
 #endif
 
-char *WORDLIST = NULL;
+char *WORDLIST[LISTLIM];
 int TOTALWORDS = 0;
 
 void strtolower(char *s)
@@ -223,10 +220,6 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-    while (buf == fgets(buf, sizeof buf, fp)) {
-        arrput(WORDLIST, strdup(buf));
-    }
-
 	for (i = 0; i < LISTLIM && buf == fgets(buf, sizeof buf, fp); i++) {
 		buf[strlen(buf) - 1] = 0;
 
@@ -304,12 +297,6 @@ int main(int argc, char **argv)
 		printentries(&printidx, printamt); // we literally don't do anything special to print a diverse set of guesses
 
 		guess++;
-	}
-
-	if (guess < 6) {
-		printf("You win! Congrats!\n");
-	} else {
-		printf("You lost even with cheats? Kinda cringe.");
 	}
 
 	return 0;
